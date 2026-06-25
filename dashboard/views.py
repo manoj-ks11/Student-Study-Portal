84f2688310c9e1c3f89623d837193e8b82d68d45
Manoj Kumar Senapaty <manoj.senapaty97@gmail.com>
2026-06-25 23:59:42 +0530

    register feature

--- dashboard/views.py
+++ dashboard/views.py
@@ -407,3 +407,18 @@ def conversion(request):
     }
 
     return render(request, "dashboard/conversion.html", context)
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request,f"Account Created for {username}!!")
+            # redirect("login")
+    else:
+        form = UserRegistrationForm()
+    context = {
+        'form':form
+    }
+    return render(request, "dashboard/register.html",context)
